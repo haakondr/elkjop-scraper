32f8c67406b303e949486d646699f999873b5ded
Håkon Drolsum Røkenes <hakon.drolsum.rokenes@niva.no>
2021-06-08 19:10:30 +0200

    first working version of postprocessing script. Outputs list of stores

--- main.ts
+++ main.ts
@@ -3,15 +3,15 @@ import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 // The filename is the first invocation argument
 const filename = Deno.args[0] // Same name as downloaded_filename
-const text = Deno.readTextFile(filename);
+const text = await Deno.readTextFile(filename);
 
 const doc = new DOMParser().parseFromString(text, "text/html")!;
 
 const script = doc.querySelector("script")!;
 
-const scriptLines = script.split("\n");
+const scriptLines = script.textContent.split("\n");
 
-const stores = scriptLines.filter(line => line.includes("StoreCodeName"))
+const stores = scriptLines.filter((line: string) => line.includes("StoreCodeName"))
 
 console.log(stores)
 
